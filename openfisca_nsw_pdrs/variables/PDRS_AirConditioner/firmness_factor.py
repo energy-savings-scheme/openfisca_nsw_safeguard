2e67521316119c2be0b9cde2df372eb9b9814064
Yuguang Ipsen <Yuguang.Ipsen@gmail.com>
2021-03-18 14:29:28 +1100

    Add//firmness factor completes

--- openfisca_nsw_pdrs/variables/PDRS_AirConditioner/firmness_factor.py
+++ openfisca_nsw_pdrs/variables/PDRS_AirConditioner/firmness_factor.py
@@ -6,14 +6,20 @@ from openfisca_nsw_base.entities import Building
 
 # Where to put global constants ? Do they need to be displayed?
 
-RATIO_WEEKDAYS_TO_FULLWEEKS = float(5/7)
-CONTRIBUTION_FACTOR = float(1)
-NUMBER_OF_PEAK_WINDOW_HOURS = float(6*105)
 
 class zone_type(Enum):
     hot="Hot"
     average="Average"
     cold="Cold"
+
+class PDRS__Appliance__zone_type(Variable):
+    entity=Building
+    value_type=Enum
+    possible_values=zone_type
+    default_value=zone_type.average
+    definition_period=ETERNITY
+    reference="Clause **"
+    label="What is the Zone type of the area?" 
 
 
 class installation_purpose(Enum):
@@ -22,27 +28,45 @@ class installation_purpose(Enum):
 
 
 class PDRS__Appliance__installation_purpose(Variable):
-    # name="Appliance Installation Local Type" #alias
     entity=Building
     value_type=Enum
     possible_values=installation_purpose
     default_value=installation_purpose.residential
     definition_period=ETERNITY
     reference="Clause **"
-    label="Is the air-conditioner(s) installed for Residential or Commercial purpose?" #wording as form input. check with Steve
+    label="Is the air-conditioner(s) installed for Residential or Commercial purpose?" #wording as form input. 
 
 
-# entity is not compulsory?
 
-class PDRS__Air_Conditioner__load_factor(Variable):
-    # name="Load Factor in computation of firmness factor" #specific to AC or general?
-    reference=""
+class PDRS__Air_Conditioner__duration_factor(Variable):
+    reference='computing duration factor during the peak hour usage as part of the firmness factor in PDRS Air Conditioner savings.'
     entity=Building
     value_type=float
     definition_period=ETERNITY
 
-    ##got the table: put it in
-    # def formula(appliance, parameters):
-    #     installation_purpose = appliance('PDRS__Appliance__installation_purpose')
+    def formula(building, period, parameters):
+        installation_purpose=building('PDRS__Appliance__installation_purpose', period)
+        zone_type = building('PDRS__Appliance__zone_type', period)
+        operation_hrs = parameters(period).AC_hours_of_operation_by_zone_table[zone_type]
+        ratio = parameters(period).AC_peak_operation_hrs_to_all_operation_hrs_by_zone_table[zone_type]
+        weekdays_ratio = float(5/7)
+        peak_hours = parameters(period).AC_related_constants.NUMBER_OF_PEAK_WINDOW_HOURS
 
 
+        return operation_hrs[installation_purpose]*ratio[installation_purpose]*weekdays_ratio/peak_hours
+
+
+class PDRS__Air_Conditioner__firmness_factor(Variable):
+    entity=Building
+    value_type=float
+    definition_period=ETERNITY
+
+    def formula(building, period, parameters):
+        installation_purpose=building('PDRS__Appliance__installation_purpose', period)
+        zone_type = building('PDRS__Appliance__zone_type', period)
+        load_factor = parameters(period).AC_load_factors_table[installation_purpose]
+        contribution_factor = parameters(period).AC_related_constants.CONTRIBUTION_FACTOR
+        ## call the duration factor computed from above? How to call the formula under PDRS__Air_Conditioner__duration_factor ?
+        duration_factor=building('PDRS__Air_Conditioner__duration_factor', period)
+
+        return contribution_factor*load_factor*duration_factor

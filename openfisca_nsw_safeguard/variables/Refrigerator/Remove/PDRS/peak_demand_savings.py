5deaa8777771de56c5cc1164fdec2869d03b723a
YuguangIpsen <yuguang.fan@gmail.com>
2021-04-29 13:57:13 +1000

    refactor Remove Fridge acitivty

--- openfisca_nsw_safeguard/variables/Refrigerator/Remove/PDRS/peak_demand_savings.py
+++ openfisca_nsw_safeguard/variables/Refrigerator/Remove/PDRS/peak_demand_savings.py
@@ -5,6 +5,29 @@ from openfisca_nsw_base.entities import Building
 
 from openfisca_nsw_safeguard.regulation_reference import PDRS_2022
 
+
+class PDRS__ROOA__firmness_factor(Variable):
+    entity = Building
+    value_type = float
+    definition_period = ETERNITY
+    metadata = {
+        'alias': "Firmness Factor",
+        'activity-group': "Removal Of Old Appliances",
+        'activity-name': "Removal of a Spare Refrigerator or Freezer",
+        'variable-type': "intermediary",
+        "regulation_reference": PDRS_2022["X", "X.7"]
+    }
+
+    def formula(building, period, parameters):
+        load_factor = parameters(
+            period).PDRS.ROOA_fridge.ROOA_related_constants.LOAD_FACTOR
+        contribution_factor = parameters(
+            period).PDRS.PDRS_wide_constants.CONTRIBUTION_FACTOR
+        duration_factor = parameters(
+            period).PDRS.ROOA_fridge.ROOA_related_constants.DURATION_FACTOR
+        return contribution_factor*load_factor*duration_factor
+
+
 class PDRS__ROOA__peak_demand_savings(Variable):
     entity = Building
     value_type = float
@@ -16,7 +39,7 @@ class PDRS__ROOA__peak_demand_savings(Variable):
         'activity-group': "Removal Of Old Appliances",
         'activity-name': "Removal of a Spare Refrigerator or Freezer",
         'variable-type': "output",
-        "regulation_reference": PDRS_2022["X","X.7"]
+        "regulation_reference": PDRS_2022["X", "X.7"]
     }
 
     def formula(building, period, parameters):

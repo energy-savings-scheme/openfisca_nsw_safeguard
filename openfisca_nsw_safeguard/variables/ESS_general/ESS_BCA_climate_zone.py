84d368cd0d732ec2792ace19f56c28203389f207
Liam McCann <fromcassettes@gmail.com>
2021-09-21 04:40:46 +0000

    D1 activity definition tests and variables

--- openfisca_nsw_safeguard/variables/ESS_general/ESS_BCA_climate_zone.py
+++ openfisca_nsw_safeguard/variables/ESS_general/ESS_BCA_climate_zone.py
@@ -4,21 +4,21 @@ from openfisca_core.indexed_enums import Enum
 from openfisca_nsw_base.entities import Building
 
 class ESS_BCAClimateZone(Enum):
-    BCA_climate_zone_1 = 'Activity takes place in BCA Climate Zone 1.'
-    BCA_climate_zone_2 = 'Activity takes place in BCA Climate Zone 2.'
-    BCA_climate_zone_3 = 'Activity takes place in BCA Climate Zone 3.'
-    BCA_climate_zone_4 = 'Activity takes place in BCA Climate Zone 4.'
-    BCA_climate_zone_5 = 'Activity takes place in BCA Climate Zone 5.'
-    BCA_climate_zone_6 = 'Activity takes place in BCA Climate Zone 6.'
-    BCA_climate_zone_7 = 'Activity takes place in BCA Climate Zone 7.'
-    BCA_climate_zone_8 = 'Activity takes place in BCA Climate Zone 8.'
+    BCA_Climate_Zone_1 = 'Activity takes place in BCA Climate Zone 1.'
+    BCA_Climate_Zone_2 = 'Activity takes place in BCA Climate Zone 2.'
+    BCA_Climate_Zone_3 = 'Activity takes place in BCA Climate Zone 3.'
+    BCA_Climate_Zone_4 = 'Activity takes place in BCA Climate Zone 4.'
+    BCA_Climate_Zone_5 = 'Activity takes place in BCA Climate Zone 5.'
+    BCA_Climate_Zone_6 = 'Activity takes place in BCA Climate Zone 6.'
+    BCA_Climate_Zone_7 = 'Activity takes place in BCA Climate Zone 7.'
+    BCA_Climate_Zone_8 = 'Activity takes place in BCA Climate Zone 8.'
 
 
 class ESS__BCA_climate_zone(Variable):
     value_type = Enum
     entity = Building
     possible_values = ESS_BCAClimateZone
-    default_value = ESS_BCAClimateZone.BCA_climate_zone_2
+    default_value = ESS_BCAClimateZone.BCA_Climate_Zone_2
     definition_period = ETERNITY
     label = 'What BCA climate zone is the activity taking place in?'
     metadata={

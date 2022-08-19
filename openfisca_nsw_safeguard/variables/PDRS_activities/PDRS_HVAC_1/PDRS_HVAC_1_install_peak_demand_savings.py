bef232f3116d3f5b71b7a3482f2cc6a328d11a6c
Liam McCann <fromcassettes@gmail.com>
2022-08-19 10:07:07 +1000

    revisions to PDRS HVAC 1 method

--- openfisca_nsw_safeguard/variables/PDRS_activities/PDRS_HVAC_1/PDRS_HVAC_1_install_peak_demand_savings.py
+++ openfisca_nsw_safeguard/variables/PDRS_activities/PDRS_HVAC_1/PDRS_HVAC_1_install_peak_demand_savings.py
@@ -3,26 +3,24 @@ from openfisca_core.periods import ETERNITY
 from openfisca_core.indexed_enums import Enum
 from openfisca_nsw_base.entities import Building
 
-import numpy as np
-
 from openfisca_nsw_safeguard.regulation_reference import PDRS_2022
 
 ## detailed in PDRS activity XX
 
-class PDRS_HEAB_residential_AC_replace_peak_demand_savings(Variable):
+class PDRS_HVAC_1_peak_demand_savings(Variable):
     entity = Building
     value_type = float
     definition_period = ETERNITY
     reference = "Clause **"
-    label = "The final peak demand savings from replacing an air conditioner"
+    label = "The final peak demand savings from installing an air conditioner"
     metadata = {
-        "alias": "HEAB AC (replace) Peak Demand Savings",
-        "regulation_reference": PDRS_2022["HEAB", "AC_replace", "energy_savings"]
+        "alias": "AC Peak Demand Savings",
+        "regulation_reference": PDRS_2022["HEAB", "AC_install", "energy_savings"]
     }
 
-    def formula(building, period, parameters):
+    def formula(building, period):
         meets_all_requirements = building(
-            "PDRS_HEAB_residential_AC_replace_meets_all_requirements", period)
+            "PDRS_HVAC_1_install_meets_all_requirements", period)
 
         savings = building("PDRS_AC_peak_demand_savings", period)
 
--- openfisca_nsw_safeguard/variables/PDRS_activities/PDRS_HVAC_1/PDRS_HVAC_1_replace_requirements.py
+++ openfisca_nsw_safeguard/variables/PDRS_activities/PDRS_HVAC_1/PDRS_HVAC_1_replace_requirements.py
@@ -6,9 +6,9 @@ from openfisca_nsw_base.entities import Building
 
 from openfisca_nsw_safeguard.regulation_reference import PDRS_2022
 
-## detailed in PDRS activity XX
+# detailed in PDRS activity XX
 
-class PDRS_HEAB_residential_AC_install_meets_eligibility_requirements(Variable):
+class PDRS_HVAC_1_replace_meets_eligibility_requirements(Variable):
     value_type = bool
     entity = Building
     default_value = True
@@ -16,14 +16,12 @@ class PDRS_HEAB_residential_AC_install_meets_eligibility_requirements(Variable):
     label = 'Does the implementation meet all of the Eligibility' \
             ' Requirements defined in installing a high efficiency air conditioner for Business?'
     metadata = {
-        'alias': "HEAB AC install meets eligibility requirements",
-        "regulation_reference": PDRS_2022["HEAB", "AC_install", "eligibility"]
+        'alias': "HEAB AC replace meets eligibility requirements",
+        "regulation_reference": PDRS_2022["HEAB", "AC_replace", "eligibility"]
     }
 
 
-    
-
-class PDRS_HEAB_residential_AC_install_meets_equipment_requirements(Variable):
+class PDRS_HVAC_1_replace_meets_equipment_requirements(Variable):
     value_type = bool
     entity = Building
     default_value = False
@@ -31,8 +29,8 @@ class PDRS_HEAB_residential_AC_install_meets_equipment_requirements(Variable):
     label = 'Does the equipment meet all of the Equipment' \
             ' Requirements defined in installing a high efficiency air conditioner for Business?'
     metadata = {
-        'alias': "HEAB AC Install meets equipment requirements",
-        "regulation_reference": PDRS_2022["HEAB", "AC_install", "equipment"]
+        'alias': "HEAB AC replace meets equipment requirements",
+        "regulation_reference": PDRS_2022["HEAB", "AC_replace", "equipment"]
     }
 
     def formula(buildings, period, parameters):
@@ -40,13 +38,11 @@ class PDRS_HEAB_residential_AC_install_meets_equipment_requirements(Variable):
             'Appliance_is_registered_in_GEMS', period)
         has_warranty = buildings(
             'AC_TCSPF_or_AEER_exceeds_PDRS_benchmark', period)
-        demand_response = buildings(
-            'Appliance_demand_response_capability', period)
 
-        return is_in_GEM * has_warranty * demand_response
+        return is_in_GEM * has_warranty
 
 
-class PDRS_HEAB_residential_AC_install_meets_implementation_requirements(Variable):
+class PDRS_HVAC_1_replace_meets_implementation_requirements(Variable):
     value_type = bool
     entity = Building
     default_value = False
@@ -54,23 +50,21 @@ class PDRS_HEAB_residential_AC_install_meets_implementation_requirements(Variabl
     label = 'Does the implementation meet all of the Implementation' \
             ' Requirements defined in installing a high efficiency air conditioner for Business?'
     metadata = {
-        'alias': "HEAB AC Install meets implementation requirements",
-        "regulation_reference": PDRS_2022["HEAB", "AC_install", "implementation"]
+        'alias': "HEAB AC replace meets implementation requirements",
+        "regulation_reference": PDRS_2022["HEAB", "AC_replace", "implementation"]
     }
 
     def formula(buildings, period, parameters):
         is_installed = buildings(
             'Equipment_is_installed', period)
+        is_removed = buildings("Equipment_is_removed", period)
         performed_by_qualified_person = buildings(
             'implementation_is_performed_by_qualified_person', period)
 
-        return (
-                is_installed * 
-                performed_by_qualified_person
-                )
+        return is_installed * is_removed * performed_by_qualified_person
 
 
-class PDRS_HEAB_residential_AC_install_meets_all_requirements(Variable):
+class PDRS_HVAC_1_replace_meets_all_requirements(Variable):
     value_type = bool
     entity = Building
     default_value = False
@@ -78,15 +72,15 @@ class PDRS_HEAB_residential_AC_install_meets_all_requirements(Variable):
     label = 'Does the implementation meet all of the' \
             ' Requirements defined in installing a high efficiency air conditioner for Business?'
     metadata = {
-        'alias': "HEAB AC install meets all requirements",
+        'alias': "HEAB AC replace meets all requirements",
     }
 
-    def formula(buildings, period):
+    def formula(buildings, period, parameters):
         eligibility = buildings(
-            'PDRS_HEAB_residential_AC_install_meets_eligibility_requirements', period)
+            'PDRS_HVAC_1_replace_meets_eligibility_requirements', period)
         equipment = buildings(
-            'PDRS_HEAB_residential_AC_install_meets_equipment_requirements', period)
+            'PDRS_HVAC_1_replace_meets_equipment_requirements', period)
         implementation = buildings(
-            'PDRS_HEAB_residential_AC_install_meets_implementation_requirements', period)
+            'PDRS_HVAC_1_replace_meets_implementation_requirements', period)
 
         return implementation * eligibility * equipment

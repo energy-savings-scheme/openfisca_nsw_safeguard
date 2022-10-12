d0c87a5ae35a790e7ec33769a76d43a4b30068ad
Kate McKay <hello@katemckay.ca>
2022-10-12 15:29:29 +1100

    WH1 ESC calculations

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/WH1/WH1_ESC_variables.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/WH1/WH1_ESC_variables.py
@@ -21,6 +21,23 @@ class WH1_WH_capacity_factor(Variable):
 
 
 class WH1_HP_gas(Variable):
+    #Annual Gas Energy used by the End-User equipment
+    reference = 'Gj per year'
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+
+
+class WH1_com_peak_load(Variable):
+    #Peak daily winter load
+    reference = 'Mj per day'
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+
+
+class WH1_HP_elec(Variable):
+    #Annual Electrical Energy used by the End-User equipment
     reference = 'Gj per year'
     value_type = float
     entity = Building
@@ -29,4 +46,23 @@ class WH1_HP_gas(Variable):
 
 """ These variables use Rule tables
 """
+class WH1_postcode(Variable):
+    #Postcode
+    value_type = int
+    entity = Building
+    definition_period = ETERNITY
+
+
+class WH1_regional_network_factor(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = 'Regional Network Factor from ESS Table A24'
+    
+    def formula(buildings, period, parameters):
+        postcode = buildings('WH1_postcode', period)
+        rnf = parameters(period).PDRS.table_A24_regional_network_factor
+        return rnf.calc(postcode)  # This is a built in OpenFisca function that \
+        # is used to calculate a single value for regional network factor based on a zipcode provided
+
 

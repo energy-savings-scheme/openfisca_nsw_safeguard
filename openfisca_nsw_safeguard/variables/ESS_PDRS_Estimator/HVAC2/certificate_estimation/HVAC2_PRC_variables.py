10ced81a9ffbf500b74c5e45266612ef6222708c
Kate McKay <hello@katemckay.ca>
2022-10-18 10:45:40 +1100

    variable cleanup

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/HVAC2/certificate_estimation/HVAC2_PRC_variables.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/HVAC2/certificate_estimation/HVAC2_PRC_variables.py
@@ -20,7 +20,7 @@ class HVAC2_input_power(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
-    label = "Rated cooling input power at 35C as recorded in the GEMS registry"
+    label = "Input power"
     metadata = {
         'display_question': 'Rated cooling input power at 35C as recorded in the GEMS registry'
     }

f9d6a43749b37cb0ce227540948eb1ac6007906f
Kate McKay <hello@katemckay.ca>
2022-10-28 13:46:21 +1100

    RF2 PRC calculation

---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/RF2/certificate_estimation/RF2_PRC_calculation.py
@@ -0,0 +1,24 @@
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+
+import numpy as np
+
+class RF2_baseline_input_power(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = 'Input Factor'
+    metadata = {
+        'variable-type': 'output'
+    }
+
+    def formula(buildings, period, parameters):
+      total_energy_consumption = buildings('RF2_total_energy_consumption', period)
+      af = buildings('RF2_af', period)
+      baseline_EEI = buildings('', period)
+      product_EEI = buildings('', period)
+
+      baseline_input_power = total_energy_consumption * af * [baseline_EEI / product_EEI] / 24
+      return baseline_input_power
---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/RF2/certificate_estimation/RF2_PRC_variables.py
@@ -0,0 +1,28 @@
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+
+import numpy as np
+
+
+""" Parameters for RF2 PRC Calculation
+"""
+
+""" Values shared with ESC variables RF2_ESC_variables 
+    RF2_baseline_EEI
+    RF2_product_EEI
+"""
+
+
+"""  These variables use GEMS Registry data
+"""
+
+
+
+
+
+
+""" These variables use Rule tables
+"""
+

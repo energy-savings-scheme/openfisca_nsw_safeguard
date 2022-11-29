82ee91ba2fc42b5066bdbeda3162bf7b8eb8a329
Kate McKay <hello@katemckay.ca>
2022-11-30 10:25:16 +1100

    SYS2 ESC eligibility adjustments all tests passing

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/SYS2/certificate_estimation/SYS2_PRC_variables.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/SYS2/certificate_estimation/SYS2_PRC_variables.py
@@ -132,15 +132,12 @@ class SYS2_input_power_ESCS_eligibility_int(Variable):
 
     def formula(buildings, period, parameters):
       input_power_eligibility = buildings('SYS2_input_power_eligibility', period)
-      pool_pump_type_single = buildings('SYS2_pool_pump_type', period)
       input_power_ESC_eligibility_int = np.select([
             (input_power_eligibility == SYS2InputPowerEligibility.pump_under_100w),
             (input_power_eligibility == SYS2InputPowerEligibility.pump_100w_to_599w),
             (input_power_eligibility == SYS2InputPowerEligibility.pump_600w_to_1700w),
-            (input_power_eligibility == SYS2InputPowerEligibility.pump_1701w_to_2500w) * (pool_pump_type_single == SYS2PoolPumpType.single_speed_pool_pump),
-            (input_power_eligibility == SYS2InputPowerEligibility.pump_1701w_to_2500w) * np.logical_not(pool_pump_type_single == SYS2PoolPumpType.single_speed_pool_pump),
-            (input_power_eligibility == SYS2InputPowerEligibility.pump_2501w_to_3450w) * (pool_pump_type_single == SYS2PoolPumpType.single_speed_pool_pump),
-            (input_power_eligibility == SYS2InputPowerEligibility.pump_2501w_to_3450w) * np.logical_not(pool_pump_type_single != SYS2PoolPumpType.single_speed_pool_pump),
+            (input_power_eligibility == SYS2InputPowerEligibility.pump_1701w_to_2500w),
+            (input_power_eligibility == SYS2InputPowerEligibility.pump_2501w_to_3450w),
             (input_power_eligibility == SYS2InputPowerEligibility.pump_more_than_3451w),
         ],
         [
@@ -149,12 +146,9 @@ class SYS2_input_power_ESCS_eligibility_int(Variable):
             True,
             True,
             False,
-            False,
-            False,
             False
         ])
       print('ESC Input power eligibility', input_power_eligibility)
-      print('ESC Pool pump type', pool_pump_type_single)
       print('ESC input power', input_power_ESC_eligibility_int)
       return input_power_ESC_eligibility_int
 

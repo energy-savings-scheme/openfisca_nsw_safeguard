ae287a02f48c7e5f7d4dc7b1b6e45aa8f19eecea
Kate McKay <hello@katemckay.ca>
2022-12-08 15:12:57 +1100

    Base eligibility eligibility clauses

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/ESS_PDRS_base_scheme/ESS_PDRS_base_eligibility_variables.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/ESS_PDRS_base_scheme/ESS_PDRS_base_eligibility_variables.py
@@ -10,17 +10,18 @@ class Base_reduces_energy_consumption(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    #label = 'Does the activity reduce energy consumption?'
     metadata = {
         'display_question' : 'Does the activity reduce energy consumption compared to what would have been consumed?',
         'sorting' : 1,
-        'clause' : 'In ESS Clause 5.3, it states that a Recognised Energy Saving Activity is any activity that meets all of the following criteria: it increases the efficiency of energy consumption, by modifying End-User Equipment or the usage of End-User Equipment (including by
-installing additional components) with the result that there is a reduction in the
-consumption of energy compared to what would have otherwise been consumed, it replacing End-User Equipment with other End-User Equipment that consumes less
-energy, subject to clause 5.3A, installing New End-User Equipment that consumes less energy than other comparable
-End-User Equipment of the same type, function, output or service, subject to clause
-5.3B, or removing End-User Equipment with the result that there is a reduction in the
-consumption of energy compared'
+        'eligibility_clause' : """In ESS Clause 5.3, it states that a Recognised Energy Saving Activity is any activity that meets all of the following criteria: \n
+        (a) it increases the efficiency of energy consumption, by: \n
+        (i) modifying End-User Equipment or the usage of End-User Equipment (including by installing additional components) with the result that there is a reduction in the
+        consumption of energy compared to what would have otherwise been consumed;\n
+        (ii) replacing End-User Equipment with other End-User Equipment that consumes less energy, subject to clause 5.3A;\n
+        (iii) installing New End-User Equipment that consumes less energy than other comparable End-User Equipment of the same type, function, output or service, subject to clause
+        5.3B; or \n
+        (iv) removing End-User Equipment with the result that there is a reduction in the consumption of energy compared to what would have otherwise been consumed,
+        subject to clause 5.3A."""
     }
 
 
@@ -29,11 +30,12 @@ class Base_provides_capacity_to_reduce_demand(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    #label = 'Does the activity provide the capacity to reduce demand?'
     metadata = {
         'label' : 'The Peak Demand Reduction period is 2.30pm to 8.30pm AEST',
         'display_question' : 'Does the activity provide capacity to reduce demand during the Peak Demand Reduction period?',
-        'sorting' : 2
+        'sorting' : 2,
+        'eligibility_clause' : """ In PDRS Clause 5.1, it states a Recognised Peak Activity is an activity that:\n
+        (a) provides capacity to reduce peak demand during the Peak Demand Reduction Period."""
     }
 
 
@@ -42,10 +44,12 @@ class Base_implemented_activity(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    #label = 'Has implementation already occurred?'
     metadata = {
         'display_question' : 'Has implementation already occurred?',
-        'sorting' : 3
+        'sorting' : 3,
+        'eligibility_clause' : """In ESS Clause 5.3A(b) it states that the replacement or removal of End-User Equipment only constitutes a Recognised Energy
+        Saving Activity if the Implementation Date is on or after 15 May 2016, disposes of that End-User Equipment appropriately.\n
+        In PDRS Clause5.1(d), it states that a Recognised Peak Activity is an activity that has an Implementation Date on or after 1 April 2022."""
     }
 
 
@@ -56,7 +60,10 @@ class Base_implementation_after_1_April_2022(Variable):
     #label = 'What date did the implementation occur?'
     metadata = {
         'display_question' : 'What date did the implementation occur?',
-        'sorting' : 4
+        'sorting' : 4,
+        'eligibility_clause' : """In ESS Clause 5.3A(b) it states that the replacement or removal of End-User Equipment only constitutes a Recognised Energy
+        Saving Activity if the Implementation Date is on or after 15 May 2016, disposes of that End-User Equipment appropriately.\n
+        In PDRS Clause5.1(d), it states that a Recognised Peak Activity is an activity that has an Implementation Date on or after 1 April 2022."""
     }
 
 
@@ -65,10 +72,10 @@ class Base_lawful_activity(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    #label ='Was your activity lawful in NSW on the implementation date?'
     metadata = {
         'display_question' : 'Was the activity lawful in NSW on the implementation date?',
-        'sorting' : 5
+        'sorting' : 5,
+        'eligibility_clause' : """In PDRS Clause 5.1(c) it states tht a Recognised Peak Activity is an activity that is not unlawful in New South Wales on the Implementation Date;"""
     }
 
 
@@ -77,8 +84,8 @@ class Base_registered_ACP(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    label = 'An Accredited Certificate Provider is a person who is accredited by the Statistical Society of Australia Inc. at the time of carrying out the verification in accordance with ESS clause 8.9.7(e), and
-            who is accepted by the Scheme Administrator for the purposes of the Rule.''
+    label = """An Accredited Certificate Provider is a person who is accredited by the Statistical Society of Australia Inc. at the time of carrying out 
+               the verification in accordance with ESS clause 8.9.7(e), and who is accepted by the Scheme Administrator for the purposes of the Rule."""
     metadata = {
         'display_question' : 'Are you an Accredited Certificate Provider?',
         'sorting' : 6
@@ -90,11 +97,14 @@ class Base_engaged_ACP(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    #label = 'Did you engage an Accredited Certificate Provider prior to the implementation date?'
     metadata = {
         'display_question' : 'Was an Accredited Certificate Provider engaged prior to the implementation date?',
         'sorting' : 7,
-        'conditional' : 'True'
+        'conditional' : 'True',
+        'eligibility_clause' : """In ESS Clause 6.2, it states that an Accredited Certificate Provider may only create Energy Savings Certificates in respect of
+                                  the Energy Savings for an Implementation where:\n
+                                  (a) the Accredited Certificate Provider is the Energy Saver for those Energy Savings as at the Implementation Date; and \n
+                                  (b) the Accredited Certificate Provider’s Accreditation Date for that Recognised Energy Saving Activity is prior to the Implementation Date."""
     }
 
 
@@ -103,7 +113,6 @@ class Base_removing_or_replacing(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    #label = 'Are you removing or replacing End-User equipment?'
     metadata = {
         'display_question' : 'Is the activity removing or replacing End-User equipment?',
         'sorting' : 8
@@ -115,11 +124,12 @@ class Base_resold_reused_or_refurbished(Variable):
     entity = Building
     default_value = False
     definition_period = ETERNITY
-    #label = 'Is the removed End-User equipment re-sold, refurbished or re-used?'
     metadata = {
         'display_question' : 'Is the removed End-User equipment re-sold, refurbished or re-used?',
         'sorting' : 9,
-        'conditional': 'True'
+        'conditional' : 'True',
+        'eligibility_clause' : """In PDRS Clause 5.3(a), it states that the replacement or removal of End-User Equipment only constitutes a Recognised Peak Activity
+        if the End-User Equipment is not refurbished, re-used or resold."""
     }
 
 
@@ -128,11 +138,13 @@ class Base_disposal_of_equipment(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    #label = 'Will your End-User equipment be disposed of in accordance with legal requirements, (including by obtaining evidence for any refrigerants being disposed of or recycled)?'
     metadata = {
         'display_question' : 'Will the End-User equipment be disposed of in accordance with legal requirements, (including by obtaining evidence for any refrigerants being disposed of or recycled)?',
         'sorting' : 10,
-        'conditional': 'True'
+        'conditional': 'True',
+        'eligibility_clause' : """In PDRS Clause 5.3(b), it states that the replacement or removal of End-User Equipment only constitutes a Recognised Peak Activity if it is disposed of in accordance 
+        with legal requirements imposed through a statutory or regulatory instrument of the Commonwealth or a State or Territory of the Commonwealth, including by obtaining evidence for any refrigerants 
+        being disposed of or recycled."""
     }
 
 
@@ -141,10 +153,11 @@ class Base_reduces_safety_levels(Variable):
     entity = Building
     default_value = False
     definition_period = ETERNITY
-    #label = 'Will your activity reduce safety levels or permanently reduce production or service levels?'
     metadata = {
         'display_question' : 'Will the activity reduce safety levels or permanently reduce production or service levels?',
-        'sorting' : 11
+        'sorting' : 11,
+        'eligibility_clause' : """In PDRS Clause 5.4(a) it states that an activity is not a Recognised Peak Activity if it results in the creation
+        of Peak Demand Reduction Capacity by reducing safety levels or permanently reducing production or service levels."""
     }
 
 
@@ -156,7 +169,8 @@ class Base_greenhouse_emissions_increase(Variable):
     #label = 'Will your activity lead to a net increase in greenhouse emissions?'
     metadata = {
         'display_question': 'Will the activity lead to a net increase in greenhouse emissions?',
-        'sorting' : 12
+        'sorting' : 12,
+        'eligibility_clause' : """In PDRS Clause 5.4(b) it states that an activity is not a Recognised Peak Activity if it contributes to a net increase in greenhouse gas emissions."""
     }
 
 
@@ -165,10 +179,12 @@ class Base_meets_mandatory_requirement(Variable):
     entity = Building
     default_value = False
     definition_period = ETERNITY
-    label = 'Is your activity being undertaken to comply with any mandatory legal requirements?'
     metadata = {
         'display_question': 'Is the activity being undertaken to comply with any mandatory legal requirements?',
-        'sorting' : 13
+        'sorting' : 13,
+        'eligibility_clause' : """In PDRS Clause 5.4(c) it states that an activity is not a Recognised Peak Activity if it is undertaken to comply with any mandatory legal requirement imposed 
+        through a statutory or regulatory instrument of the Commonwealth or a State or Territory of the Commonwealth, including but not limited to National Construction Code and BASIX affected 
+        development requirements, except for alterations, enlargements or extensions of a BASIX affected development as defined in clause 3(1)(c) of the Environmental Planning and Assessment Regulation 2021."""
     }
 
 
@@ -177,11 +193,14 @@ class Base_basix_affected_development(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    label = 'Is your activity an alteration, enlargement or extension of a BASIX affected development?'
     metadata = {
         'display_question': 'Is the activity an alteration, enlargement or extension of a BASIX affected development?',
         'sorting' : 14,
-        'conditional': 'True'
+        'conditional': 'True',
+        'eligibility_clause' : """In PDRS Clause 5.4(c) it states that an activity is not a Recognised Peak Activity if it is undertaken to comply with any mandatory legal requirement imposed 
+        through a statutory or regulatory instrument of the Commonwealth or a State or Territory of the Commonwealth, including but not limited to National Construction Code and BASIX affected 
+        development requirements, except for alterations, enlargements or extensions of a BASIX affected development as defined in clause 3(1)(c) of the Environmental Planning and Assessment Regulation 2021."""
+
     }
 
 
@@ -193,7 +212,9 @@ class Base_prescribed_transmission_service(Variable):
     label = 'Is your activity a Standard Control Service or Prescribed Transmission service undertaken by a Network Service Provider?'
     metadata = {
         'display_question': 'Is the activity a Standard Control Service or Prescribed Transmission service undertaken by a Network Service Provider?',
-        'sorting' : 15
+        'sorting' : 15,
+        'eligibility_clause' : """In PDRS Clause 5.4(d) it states that an activity is not a Recognised Peak Activity if it is a Standard Control Service or Prescribed Transmission Service 
+        undertaken by a Network Service Provider in accordance with the National Electricity Rules under the National Electricity (NSW) Law, except if the activity is a Non-Network Option."""
     }
 
 
@@ -202,10 +223,11 @@ class Base_tradeable_certificates(Variable):
     entity = Building
     default_value = False
     definition_period = ETERNITY
-    #label = 'Have you created tradeable certificates under the Renewable Energy Act?'
     metadata = {
         'display_question' : 'Is the activity eligible to create tradeable certificates under the Renewable Energy Act?',
-        'sorting' : 16
+        'sorting' : 16,
+        'eligibility_clause' : """In PDRS Clause 5.4(e) it states that an activity is not a Recognised Peak Activity if it is eligible to create tradeable certificates under the Renewable Energy (Electricity) Act 2000 (Cth),
+        except if the activity is the installation of a replacement heat pump water heater."""
     }
 
 
@@ -214,11 +236,12 @@ class Base_replacement_water_heater_certificates(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    #label = 'Is the activity the installation of a replacement heat pump water heater?'
     metadata = {
         'display_question' : 'Is the activity the installation of a replacement heat pump water heater?',
         'sorting' : 17,
-        'conditional': 'True'
+        'conditional': 'True',
+        'eligibility_clause' : """In PDRS Clause 5.4(e) it states that an activity is not a Recognised Peak Activity if it is eligible to create tradeable certificates under the Renewable Energy (Electricity) Act 2000 (Cth),
+        except if the activity is the installation of a replacement heat pump water heater."""
     }
 
 
@@ -231,5 +254,8 @@ class Base_replacement_solar_water_heater_certificates(Variable):
     metadata = {
         'display_question' : 'Is the activity the installation of a replacement solar water heater?',
         'sorting' : 18,
-        'conditional': 'True'
+        'conditional': 'True',
+        'eligibility_clause' : """In ESS Clause 5.4(g) it states that Recognised Energy Saving Activities do not include any of the following:\n
+        an activity that is eligible to create tradeable certificates under the Renewable Energy (Electricity) Act 2000 (Cth), except if the activity is the installation of a new or
+        replacement solar water heater or heat pump water heater."""
     }

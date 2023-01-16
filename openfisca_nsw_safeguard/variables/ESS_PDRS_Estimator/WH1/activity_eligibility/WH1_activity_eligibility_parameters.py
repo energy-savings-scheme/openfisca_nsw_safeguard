5c0045ddf9f4dbf863e51ac2f3a97238c7cfb0a0
alisha17 <anejaalisha37@gmail.com>
2023-01-17 04:51:14 +0530

    update variable conditions

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/WH1/activity_eligibility/WH1_activity_eligibility_parameters.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/WH1/activity_eligibility/WH1_activity_eligibility_parameters.py
@@ -181,21 +181,11 @@ class WH1_equipment_certified_by_storage_volume(Variable):
     def formula(buildings, period, parameters):
       storage_volume = buildings('WH1_storage_volume', period)
       certified_AS_NZ_2712 = buildings('WH1_certified', period)
-      
-      storage_volume = np.select(
-        [
-           storage_volume == 'Less than or equal to 700 litres',
-           storage_volume == 'More than 700 litres'
-        ],
-        [
-           WH1StorageVolume.less_than_or_equal_to_700_L,
-           WH1StorageVolume.more_than_700_L
-        ])
 
       eligible_by_storage = np.select(
         [
           (storage_volume == WH1StorageVolume.less_than_or_equal_to_700_L) * certified_AS_NZ_2712,
-          (storage_volume == WH1StorageVolume.less_than_or_equal_to_700_L) * (certified_AS_NZ_2712 is not True),
+          (storage_volume == WH1StorageVolume.less_than_or_equal_to_700_L) * np.logical_not(certified_AS_NZ_2712),
           (storage_volume == WH1StorageVolume.more_than_700_L)
         ],
         [

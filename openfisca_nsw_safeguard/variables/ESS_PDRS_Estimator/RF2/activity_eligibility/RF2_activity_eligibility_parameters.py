f82d52c4b8e384b3e7b9586d3c1ded6cbc99fa43
Kate McKay <hello@katemckay.ca>
2023-01-24 10:55:29 +1100

    Replacement installation cleanup

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/RF2/activity_eligibility/RF2_activity_eligibility_parameters.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/RF2/activity_eligibility/RF2_activity_eligibility_parameters.py
@@ -12,7 +12,7 @@ class RF2_equipment_replaced(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'Is the activity the replacement of an existing refrigerated cabinet?',
-      'sorting' : 2,
+      'sorting' : 1,
       'conditional' : 'True',
       'eligibility_clause' : """In PDRS RF2 Eligibility Requirements Clause 1 it states that this activity must be a replacement of an existing Refrigerated Cabinet (whether operational or not) with a high efficiency Refrigerated Cabinet."""
     }
@@ -25,8 +25,20 @@ class RF2_installation(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'Is the activity the installation of a new refrigerated cabinet?',
-      'sorting' : 1,
+      'sorting' : 2,
       'eligibility_clause' : """This activity is only eligible for ESCs, in PDRS RF2 Eligibility Requirements Clause 1 it states that this activity must be a replacement of an existing Refrigerated Cabinet (whether operational or not) with a high efficiency Refrigerated Cabinet."""
+    }
+
+
+class RF2_qualified_install(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Has the installation of the end-user equipment been performed or supervised by a suitably licensed person?',
+      'sorting' : 3,
+      'eligibility_clause' : """In PDRS RF2 Implementation Requirements Clause 3 it states that the activity, including the removal of the existing End-User Equipment, must be performed or supervised by a suitably Licensed person in compliance with the relevant standards and legislation."""
     }
 
 
@@ -37,7 +49,7 @@ class RF2_qualified_install_removal(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'Has the removal of the existing equipment and the installation of the end-user equipment been performed or supervised by a suitably licensed person?',
-      'sorting' : 3,
+      'sorting' : 4,
       'eligibility_clause' : """In PDRS RF2 Implementation Requirements Clause 3 it states that the activity, including the removal of the existing End-User Equipment, must be performed or supervised by a suitably Licensed person in compliance with the relevant standards and legislation."""
     }
 
@@ -49,7 +61,7 @@ class RF2_legal_disposal(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'Has the existing End-User equipment been removed and disposed of in accordance with legislation?',
-      'sorting' : 4,
+      'sorting' : 5,
       'eligibility_clause' : """In PDRS RF2 Implementation Requirements Clause 1 it states that the existing End-User Equipment must be removed and disposed of in accordance with legislation."""
     }
 
@@ -62,7 +74,7 @@ class RF2_equipment_installed(Variable):
     label = 'Is the new End-User equipment installed and operational?'
     metadata = {
         'display_question' : 'Is the new End-User equipment installed and operational?',
-        'sorting' : 5,
+        'sorting' : 6,
         'eligibility_clause' : """In PDRS RF2 Implementation Requirements Clause 2 it states that the replacement End-User Equipment must be installed in its intended place of use and operating."""
     }
 
@@ -74,7 +86,7 @@ class RF2_equipment_registered_in_GEMS(Variable):
     definition_period = ETERNITY
     metadata = {
         'display_question' : 'Is the installed End-User equipment a registered product on the GEMS registry under GEMS (refrigerated cabinets) Determination 2020?',
-        'sorting' : 6,
+        'sorting' : 7,
         'eligibility_clause' : """In PDRS RF2 Equipment Requirements Clause 1 it states that the End-User Equipment must be a Refrigerated Cabinet (RC) as defined within the terms of the Greenhouse and Energy Minimum Standards (Refrigerated Cabinets) Determination 2020."""
     }
 
@@ -86,7 +98,7 @@ class RF2_GEMS_product_class_5(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'Is the End-User equipment GEMS Product Class 5?',
-      'sorting' : 7,
+      'sorting' : 8,
       'conditional' : 'True',
       'eligibility_clause' : """In PDRS RF2 Equipment Requirements Clause 2 it states that the refrigerated cabinet must have an Energy Efficiency Index (EEI) below 81, as recorded in the GEMS Registry, with the exception of Integral Ice Cream Freezer Cabinets (class 5) which must have an EEI below 51, as recorded in the GEMS Registry."""
     }
@@ -100,7 +112,7 @@ class RF2_EEI_under_51(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'Is the product Energy Efficiency Index (EEI) as recorded in the GEMS Registry below 51?',
-      'sorting' : 8,
+      'sorting' : 9,
       'eligibility_clause' : """In PDRS RF2 Equipment Requirements Clause 2 it states that the refrigerated cabinet must have an Energy Efficiency Index (EEI) below 81, as recorded in the GEMS Registry, with the exception of Integral Ice Cream Freezer Cabinets (class 5) which must have an EEI below 51, as recorded in the GEMS Registry."""
     }
 
@@ -113,6 +125,6 @@ class RF2_EEI_under_81(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'Is the product Energy Efficiency Index (EEI) as recorded in the GEMS Registry below 81?',
-      'sorting' : 9,
+      'sorting' : 10,
       'eligibility_clause' : """In PDRS RF2 Equipment Requirements Clause 2 it states that the refrigerated cabinet must have an Energy Efficiency Index (EEI) below 81, as recorded in the GEMS Registry, with the exception of Integral Ice Cream Freezer Cabinets (class 5) which must have an EEI below 51, as recorded in the GEMS Registry."""
     }

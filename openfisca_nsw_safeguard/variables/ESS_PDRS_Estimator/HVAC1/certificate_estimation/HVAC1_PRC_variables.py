338241801c7679e79b512852bdb3b56afb6b907e
Kate McKay <hello@katemckay.ca>
2023-03-06 15:38:33 +1100

    Removed old variables

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/HVAC1/certificate_estimation/HVAC1_PRC_variables.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/HVAC1/certificate_estimation/HVAC1_PRC_variables.py
@@ -26,40 +26,7 @@ class HVAC1_input_power(Variable):
         'sorting' : 9,
         'label': 'Rated cooling input power (kW)'
     }
-
-# class HVAC1_DNSP_Options(Enum):
-#     Ausgrid = 'Ausgrid'
-#     Endeavour = 'Endeavour'
-#     Essential = 'Essential'
-
-
-# class HVAC1_DNSP(Variable):
-#      # this variable is used as the second input on all estimator certificate calculation pages
-#     value_type = Enum
-#     entity = Building
-#     possible_values = HVAC1_DNSP_Options
-#     default_value = HVAC1_DNSP_Options.Ausgrid
-#     definition_period = ETERNITY
-#     label = "Distribution Network Service Provider"
-#     metadata = {
-#         'variable-type': 'user-input',
-#         'display_question': 'Who is your Distribution Network Service Provider?',
-#         'sorting' : 2,
-#         'label': "Distribution Network Service Provider"
-#     }
-
-
-# class HVAC1_network_loss_factor(Variable):
-#     reference = 'table_A3_network_loss_factors'
-#     value_type = float
-#     entity = Building
-#     definition_period = ETERNITY
-
-#     def formula(buildings, period, parameters):
-#         distribution_district = buildings('HVAC1_DNSP', period)
-#         network_loss_factor = parameters(period).PDRS.table_A3_network_loss_factors['network_loss_factor'][distribution_district]
-#         return network_loss_factor
-
+    
 
 class HVAC1_get_network_loss_factor_by_postcode(Variable):
     value_type = float

3abde5bcf04329f2be748e7d40e0aad200c0a678
Kate McKay <hello@katemckay.ca>
2023-03-13 14:40:11 +1100

    RF2 new install all tests passing

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/WH1/activity_eligibility/WH1_activity_eligibility_parameters.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/WH1/activity_eligibility/WH1_activity_eligibility_parameters.py
@@ -26,7 +26,7 @@ class WH1_installation(Variable):
       'display_question' : 'Is the activity the installation of a new heat pump water heater?',
       'sorting' : 2,
       'conditional' : 'True',
-      'eligibility_clause' : """In PDRS WH1 the activity definition states that the activity must replace one or more existing hot water boilers or water heaters with one or more air source heat pump water heater systems."""
+      'eligibility_clause' : """This activity is not eligible for PRCs (only eligible for ESCs), in PDRS WH1 the activity definition states that the activity must replace one or more existing hot water boilers or water heaters with one or more air source heat pump water heater systems."""
     }
 
 

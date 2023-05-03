06a7d0a4bf5415bbc7c77799e8c78422b9cf0261
Kate McKay <hello@katemckay.ca>
2023-05-03 13:54:44 +1000

    Trying out a new combination all tests passing

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/ESS_PDRS_base_scheme/ESS_PDRS_base_eligibility_calculation.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/ESS_PDRS_base_scheme/ESS_PDRS_base_eligibility_calculation.py
@@ -20,11 +20,12 @@ class ESS__PDRS__ACP_base_scheme_eligibility(Variable):
     def formula(buildings, period, parameter):
         energy_consumption = buildings('Base_reduces_energy_consumption', period)
         reduce_demand = buildings('Base_provides_capacity_to_reduce_demand', period)
-        implementation_date = buildings('Implementation_date', period)
-        date_options = implementation_date.possible_values
-        planned_activity = (implementation_date == date_options.planned_activity)
-        #before_april_1 = (implementation_date == date_options.before_april_1_2022)
-        april_1_or_later = (implementation_date == date_options.april_1_2022_or_later)
+        # implementation_date = buildings('Implementation_date', period)
+        implementation_date_eligibility = buildings('Implementation_date_eligibility', period)
+        # date_options = implementation_date.possible_values
+        # planned_activity = (implementation_date == date_options.planned_activity)
+        # #before_april_1 = (implementation_date == date_options.before_april_1_2022)
+        # april_1_or_later = (implementation_date == date_options.april_1_2022_or_later)
         lawful = buildings('Base_lawful_activity', period)
         engaged_an_ACP = buildings('Base_engaged_ACP', period)
         removing_or_replacing = buildings('Base_removing_or_replacing', period)
@@ -40,7 +41,7 @@ class ESS__PDRS__ACP_base_scheme_eligibility(Variable):
         replacement_solar_hw = buildings('Base_replacement_solar_water_heater_certificates', period)
         
         # implementation date is planned activity or after april 1
-        implementation_eligible = planned_activity + april_1_or_later
+        # implementation_eligible = planned_activity + april_1_or_later
 
         # removing or replacing is YES and equipment is not resold, reused or refurbished and is disposed of appropriately
         removing_replacing_intermediary = np.logical_not(removing_or_replacing) + (removing_or_replacing * np.logical_not(resold_reused_refurbished) * appropriate_disposal)
@@ -51,7 +52,7 @@ class ESS__PDRS__ACP_base_scheme_eligibility(Variable):
         # tradeable certificates is YES and replacement heat pump water heater is YES and solar water heater is no
         tradeable_certificates_allowed = np.logical_not(tradeable_certificates) + (tradeable_certificates * replacement_hw) + (tradeable_certificates * replacement_solar_hw)
         
-        end_formula =  ( energy_consumption * reduce_demand * implementation_eligible * lawful * engaged_an_ACP *
+        end_formula =  ( energy_consumption * implementation_date_eligibility * reduce_demand * lawful * engaged_an_ACP *
                          removing_replacing_intermediary * np.logical_not(reduce_safety_levels) * np.logical_not(increase_emissions) * mandatory_allowance *
                          np.logical_not(prescribed_service) * tradeable_certificates_allowed)
         

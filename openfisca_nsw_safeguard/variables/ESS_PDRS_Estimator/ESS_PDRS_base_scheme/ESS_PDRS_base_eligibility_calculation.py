d5c6285c7421c465b367b275837def7a99cce335
Kate McKay <hello@katemckay.ca>
2023-05-03 11:58:53 +1000

    removed implementation occured question

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/ESS_PDRS_base_scheme/ESS_PDRS_base_eligibility_calculation.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/ESS_PDRS_base_scheme/ESS_PDRS_base_eligibility_calculation.py
@@ -20,11 +20,10 @@ class ESS__PDRS__ACP_base_scheme_eligibility(Variable):
     def formula(buildings, period, parameter):
         energy_consumption = buildings('Base_reduces_energy_consumption', period)
         reduce_demand = buildings('Base_provides_capacity_to_reduce_demand', period)
-        activity_implemented = buildings('Base_implemented_activity', period)
         implementation_date = buildings('Implementation_date', period)
         date_options = implementation_date.possible_values
         planned_activity = (implementation_date == date_options.planned_activity)
-        # before_april_1 = (implementation_date == date_options.before_april_1_2022)
+        #before_april_1 = (implementation_date == date_options.before_april_1_2022)
         april_1_or_later = (implementation_date == date_options.april_1_2022_or_later)
         lawful = buildings('Base_lawful_activity', period)
         engaged_an_ACP = buildings('Base_engaged_ACP', period)
@@ -40,8 +39,8 @@ class ESS__PDRS__ACP_base_scheme_eligibility(Variable):
         replacement_hw = buildings('Base_replacement_water_heater_certificates', period)
         replacement_solar_hw = buildings('Base_replacement_solar_water_heater_certificates', period)
         
-        # activity implemented is YES and the implementation date is planned activity or after april 1
-        implementation_eligible = (np.logical_not(activity_implemented) * planned_activity) + (activity_implemented * april_1_or_later)
+        # implementation date is planned activity or after april 1
+        implementation_eligible = planned_activity + april_1_or_later
 
         # removing or replacing is YES and equipment is not resold, reused or refurbished and is disposed of appropriately
         removing_replacing_intermediary = np.logical_not(removing_or_replacing) + (removing_or_replacing * np.logical_not(resold_reused_refurbished) * appropriate_disposal)

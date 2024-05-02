93dd98dfb6a547b1cf9d3d35ab76909cff54c7a0
SelvaRC <selva.chettiar@environment.nsw.gov.au>
2024-05-02 16:26:34 +1000

    D19 certificate in progress

---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/D19_ESSJun24/certificate_estimation/D19_ESSJun24_ESC_variables.py
@@ -0,0 +1,185 @@
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+
+import numpy as np
+
+
+""" Parameters for D19 ESC Calculation
+    These variables use Rule tables and IPART Registry data
+"""
+
+
+class D19_ESSJun24_PDRS__postcode(Variable):
+    value_type = int
+    entity = Building
+    definition_period = ETERNITY
+    metadata={
+        'variable-type' : 'user-input',
+        'alias' : 'PDRS Postcode',
+        'display_question' : 'Postcode where the installation has taken place',
+        'sorting' : 1,
+        'label': 'Postcode'
+    }
+
+
+ class D19_ESSJun24_get_HP_zone_by_postcode(Variable): 
+    value_type = int
+    entity = Building
+    definition_period = ETERNITY
+    
+    def formula(building, period, parameters):
+        BCA_climate_zone = building('D19_ESSJun24_PDRS__postcode', period)
+        heat_pump_zone = parameters(period).ESS.ESS_general.Postcode_zones_air_source_heat_pumps
+        heat_pump_zone_int = heat_pump_zone.calc(postcode)
+
+        return heat_pump_zone_int
+
+
+class D19_ESSJun24_regional_network_factor(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = 'Regional Network Factor from ESS Table A24'
+    
+    def formula(buildings, period, parameters):
+        postcode = buildings('D19_ESSJun24_PDRS__postcode', period)
+        rnf = parameters(period).PDRS.table_A24_regional_network_factor
+        return rnf.calc(postcode)
+
+
+class D19_ESSJun24_replacement_activity(Variable):
+    value_type = bool
+    default_value = True
+    entity = Building
+    definition_period = ETERNITY
+    metadata = {
+        'variable-type': 'user-input',
+        'label': 'Replacement or new installation activity',
+        'display_question': 'Is the activity the replacement of existing equipment?',
+        'sorting' : 2
+    }
+
+
+class D19_ESSJun24_System_Size(Enum):
+    system_size_small = 'Small'
+    system_size_medium = 'Medium'
+
+
+class D19_ESSJun24_system_size(Variable):
+    value_type = Enum
+    entity = Building
+    definition_period = ETERNITY
+    possible_values = D19_ESSJun24_System_Size
+    default_value = D19_ESSJun24_System_Size.system_size_small
+    metadata = {
+      'variable-type': 'user-input',
+      'label': 'System Size',
+      'display_question' : 'Thermal peak load size',
+      'sorting' : 3
+    }
+
+
+class D19_ESSJun24_system_size_int(Variable):
+    value_type = str
+    entity = Building
+    definition_period = ETERNITY
+
+    def formula(buildings, period, parameters):
+        system_size = buildings('D19_ESSJun24_system_size', period)
+        system_size_int = np.select(
+            [
+                (system_size == D19_ESSJun24_System_Size.system_size_small),
+                (system_size == D19_ESSJun24_System_Size.system_size_medium)
+            ],
+            [
+                'small',
+                'medium'
+            ])
+        return system_size_int
+
+
+class D19_ESSJun24_Baseline_A(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+
+    def formula(buildings, period, parameters):
+        system_size = buildings('D19_ESSJun24_system_size_int', period)
+        heat_pump_zone = buildings('D19_ESSJun24_get_HP_zone_by_postcode', period)
+
+        heat_pump_zone_str = np.select(
+            [
+                heat_pump_zone == 3,
+                heat_pump_zone == 5
+            ],
+            [
+                'heat_pump_zone_3',
+                'heat_pump_zone_5'
+            ])
+        
+        Baseline_A = parameters(period).ESS.HEER.table_D19_1_ESSJun24['baseline_energy_consumption'][heat_pump_zone_str][system_size]['Baseline_A']
+        return Baseline_A
+
+
+class D19_ESSJun24_adjustment_coefficient(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    
+    def formula(buildings, period, parameters):
+        system_size = buildings('D19_ESSJun24_system_size_int', period)
+        heat_pump_zone = buildings('D19_ESSJun24_get_HP_zone_by_postcode', period)
+
+        heat_pump_zone_str = np.select(
+            [
+                heat_pump_zone == 3,
+                heat_pump_zone == 5
+            ],
+            [
+                'heat_pump_zone_3',
+                'heat_pump_zone_5'
+            ])
+        
+        adjustment_coefficient = parameters(period).ESS.HEER.table_D19_1_ESSJun24['baseline_energy_consumption'][heat_pump_zone_str][system_size]['adjustment_coefficient']
+        return adjustment_coefficient        
+
+
+class D19_ESSJun24_Bs(Variable):
+    reference = 'Gj per year'
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    metadata = {
+        'display_question': 'Annual supplementary energy used by the installed End-User Equipment',
+        'sorting' : 4,
+        'label': 'Bs (GJ/year)',
+        'variable-type': 'input'
+    }
+
+
+class D19_ESSJun24_Be(Variable):
+    reference = 'Gj per year'
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    metadata = {
+        'display_question': 'Annual electrical energy used by the auxiliary equipment',
+        'sorting' : 5,
+        'label': 'Be (GJ/year)',
+        'variable-type': 'input'
+    }
+
+
+class D19_Baseline_B(Variable):
+    #baseline B is also the deemed activity gas savings value
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    
+    def formula(buildings, period, parameters):
+        system_size = buildings('D19_system_size_int', period)
+
+        baseline_B = parameters(period).ESS.HEER.table_D19_1['baseline_energy_consumption'][system_size]['baseline_B']
+        return baseline_B

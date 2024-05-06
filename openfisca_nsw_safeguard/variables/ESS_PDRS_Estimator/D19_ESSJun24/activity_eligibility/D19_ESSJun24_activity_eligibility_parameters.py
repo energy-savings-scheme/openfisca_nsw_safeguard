efbafeb3084a390bf906ec2a902ac476cd14db85
SelvaRC <selva.chettiar@environment.nsw.gov.au>
2024-05-06 15:52:59 +1000

    D19 activity eligibility parameters

---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/D19_ESSJun24/activity_eligibility/D19_ESSJun24_activity_eligibility_parameters.py
@@ -0,0 +1,100 @@
+import numpy as np
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+
+
+
+class D19_ESSJun24_equipment_replaced(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Is the activity the replacement of an existing gas water heater with an (air source) heat pump water heater?',
+      'sorting' : 1,
+      'eligibility_clause' : """A new installation is not eligible under ESS Activity D19. In ESS D19 the activity definition states that the activity must replace an existing gas water heater with an (air source) heat pump water heater. 
+                                ESS D19 Eligibility Requirements Clause 1 the existing gas water heater must be a gas storage or instantaneous water heater."""
+    }
+
+
+class D19_ESSJun24_engaged_ACP(Variable):
+    #replacement of an electric hot water heater or boiler is a different activity under the ESS (D17)
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Will an Accredited Certificate Provider be engaged before the implementation date?',
+      'sorting' : 2,
+      'eligibility_clause' : """In ESS Clause 6.2 it states that an Accredited Certificate Provider may only create Energy Savings Certificates in respect of the Energy Savings for an Implementation where: 
+                                (a) the Accredited Certificate Provider is the Energy Saver for those Energy Savings as at the Implementation Date; and 
+                                (b) the Accredited Certificate Provider’s Accreditation Date for that Recognised Energy Saving Activity is prior to the Implementation Date."""
+    }
+
+
+class D19_ESSJun24_minimum_payment(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+        'display_question' : 'Are you aware that you are required to make a minimum payment towards the cost of your upgrade?',
+        'sorting' : 3,
+        'eligibility_clause' : """In ESS Clause 9.8.1(F) it states that the Accredited Certificate Provider must have evidence satisfactory to the Scheme Administrator that the Purchaser has paid for the Implementation, assessment and other associated works carried out at the Site a Net Amount of:
+                                  i. at least $200 (excluding GST) for each item of End-User Equipment installed as part of an Implementation using any of Activity Definitions D5, D16, D17, D18, D19, D20 and D2."""
+    }
+
+
+class D19_ESSJun24_equipment_removed(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+        'display_question' : 'Has the existing equipment been removed?',
+        'sorting' : 4,
+        'eligibility_clause' : """In ESS D19 Implementation Requirements Clause 1 it states that the existing End-User Equipment must be removed."""
+    }
+
+
+class D19_ESSJun24_equipment_installed(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+        'display_question' : 'Has the replacement equipment been installed?',
+        'sorting' : 5,
+        'eligibility_clause' : """In ESS D19 Implementation Requirements Clause 2 it states that the replacement End-User Equipment must be installed at a Site in accordance with the Equipment Requirements."""
+    }
+
+
+class D19_ESSJun24_installed_by_qualified_person(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+        'display_question' : 'Will the removal of the existing equipment and the installation of the end-user equipment been performed or supervised by a suitably licensed person?',
+        'sorting' : 6,
+        'eligibility_clause' : """In ESS D19 Implementation Requirements Clause 3, it states that the activity, including the removal of any existing End-User Equipment, must be performed or supervised by a suitably Licensed person in compliance with the relevant standards and legislation."""
+    }
+
+
+class D19_ESSJun24_equipment_registered_IPART(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+        'display_question' : 'Is the installed air source heat pump a registered product on the IPART Registry?',
+        'sorting' : 7,
+        'eligibility_clause' : """In order to be listed on the IPART product register, the product has met the following three product requirements of this activity:<br /> 
+                                  1.The installed End-User Equipment must be an air source heat pump water heater as defined in AS/NZS 4234.
+                                  2.The installed End-User Equipment must be certified to AS/NZS 2712
+                                  3.The installed End-User equipment must achieve minimum annual energy savings, when determined as an air sourced heat pump using a small or medium thermal peak load System Size in accordance with AS/NZS 4232, of:
+                                    • 60% when modelled in climate zone HP3-AU.
+                                    • 60% when modelled in climate zone HP5-AU."""
+    }

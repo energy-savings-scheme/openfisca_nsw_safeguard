1614a247a92b995a14fe264820f9f8837ff2d317
Kate McKay <hello@katemckay.ca>
2024-06-05 11:40:02 +1000

    F1.2 added payment q

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/RF2_F1.2_ESSJun24/activity_eligibility/RF2_F1.2_ESSJun24_activity_eligibility_parameters.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/RF2_F1.2_ESSJun24/activity_eligibility/RF2_F1.2_ESSJun24_activity_eligibility_parameters.py
@@ -73,6 +73,18 @@ class RF2_F1_2_ESSJun24_engaged_ACP(Variable):
     }
 
 
+class RF2_F1_2_ESSJun24_minimum_payment(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Are you aware that you are required to make a minimum payment towards the cost of your upgrade?',
+      'sorting' : 6,
+      'eligibility_clause' : """In ESS Clause 9.9.1K it states that the Accredited Certificate Provider has evidence satisfactory to the Scheme Administrator that the Purchaser has paid for the Implementation, assessment and other associated works carried out at the Site a Net Amount of at least $200 (excluding GST) for each item of End-User Equipment installed as part of an Implementation using any of Activity Definitions F1.1, F1.2, F16 or F17."""
+    }
+
+
 class RF2_F1_2_ESSJun24_installed_on_site(Variable):
     value_type = bool
     entity = Building

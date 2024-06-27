df4971b2b0c3ba5765d74e952f0133f4321fc604
SelvaRC <selva.chettiar@environment.nsw.gov.au>
2024-06-27 15:51:28 +1000

    BESS1 variable PDRSAug24 TBC

---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/BESS1_PDRSAug24/certificate_estimation/BESS1_PDRSAug24_PRC_variables.py
@@ -0,0 +1,65 @@
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+
+import numpy as np
+
+
+""" Parameters for BESS1 PRC Calculation
+    These variables use Rule tables and CEC Approved Battery Registry data
+"""
+
+
+class BESS1_PDRSAug24_PDRS__postcode(Variable):
+    value_type = int
+    entity = Building
+    definition_period = ETERNITY
+    metadata={
+        'variable-type' : 'user-input',
+        'alias' : 'PDRS Postcode',
+        'display_question' : 'Postcode where the installation has taken place',
+        'sorting' : 1,
+        'label': 'Postcode'
+    }
+
+
+class BESS1_PDRSAug24_installation_activity(Variable):
+    value_type = bool
+    default_value = True
+    entity = Building
+    definition_period = ETERNITY
+    metadata = {
+        'variable-type': 'user-input',
+        'label': 'New installation or replacement activity',
+        'display_question': 'Is the activity a new installation?',
+        'sorting' : 2
+    }
+
+
+class BESS1_PDRSAug24_usable_battery_capacity(Variable):
+    value_type = int
+    entity = Building
+    definition_period = ETERNITY
+    metadata={
+        'variable-type' : 'user-input',
+        'alias' : 'battery capacity',
+        'display_question' : 'What is the battery capacity?',
+        'sorting' : 3,
+        'label': 'Usable battery capacity'
+    }
+
+
+class BESS1_PDRSAug24_get_network_loss_factor_by_postcode(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    metadata = {
+        'variable-type': 'input',
+        'label' : 'Network loss factor is calculated automatically from your postcode. If you have a 0 here, please check your postcode is correct. If the postcode has more than one distribution network service provider, we have chosen the network factor loss with the lowest value.'
+    }
+    def formula(building, period, parameters):
+        postcode = building('BESS1_PDRSAug24_PDRS__postcode', period)
+        network_loss_factor = parameters(period).PDRS.table_network_loss_factor_by_postcode
+
+        return network_loss_factor.calc(postcode)

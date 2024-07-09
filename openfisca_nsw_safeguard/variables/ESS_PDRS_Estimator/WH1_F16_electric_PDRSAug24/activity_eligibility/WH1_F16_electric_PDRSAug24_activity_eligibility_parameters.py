e6f5d38094638e71c77214cdcd56b83d41db2ff6
SelvaRC <selva.chettiar@environment.nsw.gov.au>
2024-07-09 16:33:08 +1000

    WH1 electric PDRSAug24 WIP

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/WH1_F16_electric_PDRSAug24/activity_eligibility/WH1_F16_electric_PDRSAug24_activity_eligibility_parameters.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/WH1_F16_electric_PDRSAug24/activity_eligibility/WH1_F16_electric_PDRSAug24_activity_eligibility_parameters.py
@@ -17,15 +17,39 @@ class WH1_F16_electric_PDRSAug24__equipment_replaced(Variable):
     }
 
 
-class WH1_F16_electric_PDRSAug24__equipment_removed(Variable):
+class WH1_F16_electric_PDRSAug24__existing_equipment_removed(Variable):
     value_type = bool
     entity = Building
     default_value = True
     definition_period = ETERNITY
     metadata = {
-        'display_question' : 'Will the removal of the existing equipment and the installation of the End-User equipment be performed or supervised by a suitably licensed person?',
+        'display_question' : 'Has the existing End-User equipment been removed?',
         'sorting' : 2,
-        'eligibility_clause' : """In PDRS WH1 Implementation Requirements Clause 3 it states that the activity, including the removal of any existing End-User Equipment, must be performed or supervised by a suitably Licensed person in compliance with the relevant standards and legislation."""
+        'eligibility_clause' : """In PDRS WH1 Implementation Requirements Clause 1 it states that the existing End-User Equipment must be removed."""
+    }
+
+
+class WH1_F16_electric_PDRSAug24__equipment_installed_on_site(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+        'display_question' : 'Has the replacement water heater been installed on Site?',
+        'sorting' : 3,
+        'eligibility_clause' : """In PDRS WH1 Implementation Requirements Clause 2 it states that the replacement End-User Equipment must be installed."""
+    }
+
+
+class WH1_F16_electric_PDRSAug24__qualified_install_removal(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Will the removal of the existing equipment and the installation of the End-User equipment be performed or supervised by a suitably licensed person?',
+      'sorting' : 4,
+      'eligibility_clause' : """In PDRS WH1 Implementation Requirements Clause 3 it states that the activity, including the removal of any existing End-User Equipment, must be performed or supervised by a suitably Licensed person in compliance with the relevant standards and legislation."""
     }
 
 
@@ -35,9 +59,9 @@ class WH1_F16_electric_PDRSAug24__engaged_ACP(Variable):
     default_value = True
     definition_period = ETERNITY
     metadata = {
-        'display_question' : 'Will an Accredited Certificate Provider be engaged before the implementation date?',
-        'sorting' : 3,
-        'eligibility_clause' : """In ESS Clause 6.2 it states that an Accredited Certificate Provider may only create Energy Savings Certificates in respect of the Energy Savings for an Implementation where:<br />
+      'display_question' : 'Will an Accredited Certificate Provider be engaged before the implementation date?',
+      'sorting' : 5,
+      'eligibility_clause' : """In ESS Clause 6.2 it states that an Accredited Certificate Provider may only create Energy Savings Certificates in respect of the Energy Savings for an Implementation where:<br />
                                   (a) the Accredited Certificate Provider is the Energy Saver for those Energy Savings as at the Implementation Date; and <br />
                                   (b) the Accredited Certificate Provider’s Accreditation Date for that Recognised Energy Saving Activity is prior to the Implementation Date."""
     }
@@ -48,10 +72,10 @@ class WH1_F16_electric_PDRSAug24__minimum_payment(Variable):
     entity = Building
     default_value = True
     definition_period = ETERNITY
-    metadata = {
+    metadata = {  
       'display_question' : 'Are you aware that you are required to make a minimum payment towards the cost of your upgrade?',
-      'sorting' : 4,
-      'eligibility_clause' : """In ESS Clause 9.9.1K it states that the Accredited Certificate Provider has evidence satisfactory to the Scheme Administrator that the Purchaser has paid for the Implementation, assessment and other associated works carried out at the Site a Net Amount of at least $200 (excluding GST) for each item of End-User Equipment installed as part of an Implementation using any of Activity Definitions F1.1, F1.2, F16 or F17."""
+      'sorting' : 6,
+      'eligibility_clause' : """In ESS Clause 9.8.1E it states that the Accredited Certificate Provider has evidence satisfactory to the Scheme Administrator that the Purchaser has paid for the Implementation, assessment and other associated works carried out at the Site a Net Amount of at least $200 (excluding GST) for each item of End-User Equipment installed as part of an Implementation using any of Activity Definitions F1.1, F1.2, F16 or F17."""
     }
 
 
@@ -62,8 +86,20 @@ class WH1_F16_electric_PDRSAug24__building_BCA_not_class_1_or_4(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'Is the End-User Equipment installed in a BCA Class 1 or 4 building?',
-      'sorting' : 5,
+      'sorting' : 7,
       'eligibility_clause' : """In PDRS WH1 Eligibility Requirements Clause 3 it states that the End-User Equipment must not be installed in a BCA Class 1 or 4 building."""
+    }
+
+
+class WH1_F16_electric_PDRSAug24__4234_certified(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {  
+      'display_question' : 'Is the End-User equipment AS/NZ 4234 certified?',
+      'sorting' : 8,
+      'eligibility_clause' : """In PDRS WH1 Equipment Requirements Clause 1 it states that The installed End-User Equipment must be an air source heat pump water heater as defined by AS/NZS 4234."""
     }
 
 
@@ -74,12 +110,28 @@ class WH1_F16_electric_PDRSAug24__scheme_admin_approved(Variable):
     definition_period = ETERNITY
     metadata = {  
       'display_question' : 'Has the installed end user equipment been accepted by the Scheme Administrator?',
-      'sorting' : 6,
-      'eligibility_clause' : """In PDRS WH1 Equipment Requirements Clause 4 it states that the installed End-User Equipment must be accepted in a manner determined by the Scheme Administrator."""
+      'sorting' : 9,
+      'eligibility_clause' : """In PDRS WH1 Equipment Requirements Clause 5 it states that the installed End-User Equipment must be accepted in a manner determined by the Scheme Administrator."""
+    }
+
+
+class WH1_F16_electric_PDRSAug24__minimum_annual_energy(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {  
+      'display_question' : 'Has the model met the 60% minimum annual energy savings requirement?',
+      'sorting' : 10,
+      'eligibility_clause' : """In PDRS WH1 Equipment Requirements Clause 2 it states that the installed End-User Equipment must achieve minimum annual energy savings, when determined in accordance with the modelling procedure published by the Scheme Administrator, of:<br />
+                                  (a) 60% when modelled in AS/NZS 4234 climate zone HP3-AU if the Site is in BCA Climate Zone 2, 3, 4, 5 or 6; and <br />
+                                  (b) 60% when modelled in AS/NZS 4234 climate zone HP5-AU if the Site is in BCA Climate Zone 7 or 8."""
     }
 
 
 class WH1_F16_electric_PDRSAug24__StorageVolume(Enum):
+    less_than_425_L = 'Less than 425 litres'
+    equal_425_L_to_700_L = '425 - 700 litres'
     less_than_or_equal_to_700_L = 'Less than or equal to 700 litres'
     more_than_700_L = 'More than 700 litres'
 
@@ -92,11 +144,12 @@ class WH1_F16_electric_PDRSAug24__storage_volume(Variable):
     definition_period = ETERNITY
     metadata = {
       'display_question' : 'What is the storage volume of the End-User equipment (litres)?',
-      'sorting' : 7,
-      'eligibility_clause' : """In PDRS WH1 Equipment Requirements Clause 3 it states the installed End-User Equipment must be certified to comply with AS/NZS 2712 if it has a storage volume less than or equal to 700L."""
+      'sorting' : 11,
+      'eligibility_clause' : """In PDRS WH1 Equipment Requirements Clause 3 it states that the installed End-User Equipment must be certified to comply with AS/NZS 2712 if it has a storage volume less than or equal to 700L.
+                                In PDRS WH1 Equipment Requirements Clause 4 it states that Each replacement heat pump as defined by AS/NZS 4234 must have a volumetric capacity of greater than 425 litres, where volumetric capacity means the total volume of water in litres that can be held in the storage tank, as defined in clause 1.5.24 of AS/NZS 2712."""
     }
 
-
+# UP TO HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 class WH1_F16_electric_PDRSAug24__storage_volume_int(Variable):
     value_type = int
     entity = Building 

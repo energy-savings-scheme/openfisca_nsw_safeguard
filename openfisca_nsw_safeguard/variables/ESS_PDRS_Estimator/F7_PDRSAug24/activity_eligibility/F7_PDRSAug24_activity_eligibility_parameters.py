c0593ccf3fa4fc5b6e62cc9cfe4218b3a2b67f8b
Kate McKay <hello@katemckay.ca>
2024-07-15 12:03:47 +1000

    Remove PRCs

---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/F7_PDRSAug24/activity_eligibility/F7_PDRSAug24_activity_eligibility_parameters.py
@@ -0,0 +1,79 @@
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+import numpy as np
+
+
+class SYS1_new_motor_for_ventilation_refrigeration(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Is the activity the installation of a new motor for a ventilation or refrigeration application?',
+      'sorting' : 1,
+      'eligibility_clause' : """In PDRS SYS1 Eligibility Requirements Clause 1 it states that the motor must be installed for use in ventilation or refrigeration applications."""
+    }
+
+
+class SYS1_equipment_registered_in_GEMS(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+        'display_question' : 'Is the new End-User equipment a registered product on the GEMS registry under GEMS (Three Phase Cage Induction Motors) Determination 2019?',
+        'sorting' : 2,
+        'eligibility_clause' : """In PDRS SYS1 Equipment Requirements Clause 2 it states that The electric motor must be a registered product under GEMS and comply with the Greenhouse and Energy Minimum Standards (Three Phase Cage Induction Motors) Determination 2019."""
+    }
+
+
+class SYS1_engaged_ACP(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+        'display_question' : 'Will an Accredited Certificate Provider be engaged before the implementation date?',
+        'sorting' : 3,
+        'eligibility_clause' : """In ESS Clause 6.2 it states that an Accredited Certificate Provider may only create Energy Savings Certificates in respect of the Energy Savings for an Implementation where:<br />
+                                  (a) the Accredited Certificate Provider is the Energy Saver for those Energy Savings as at the Implementation Date; and <br />
+                                  (b) the Accredited Certificate Provider’s Accreditation Date for that Recognised Energy Saving Activity is prior to the Implementation Date."""
+    }
+
+
+class SYS1_high_efficiency(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Is the new End-User equipment a 3-phase, high efficiency electric motor?',
+      'sorting' : 4,
+      'eligibility_clause' : """In PDRS SYS1 Equipment Requirements Clause 1 it states that the End-User Equipment must be a 3 phase electric motor rated high efficiency within the meaning of Part 5 of the Greenhouse and Energy Minimum Standards (Three Phase Cage Induction Motors) Determination 2019 when tested in accordance with subclause 6.1.3 of IEC60034-2-1."""
+    }   
+
+
+class SYS1_equipment_installed(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Has the new End-User equipment been installed?',
+      'sorting' : 5,
+      'eligibility_clause' : """In PDRS SYS1 Implementation Requirements Clause 1 it states that the electric motor must be installed."""
+    }
+
+
+class SYS1_rated_output_eligible(Variable):
+    value_type = bool
+    entity = Building
+    default_value = True
+    definition_period = ETERNITY
+    metadata = {
+      'display_question' : 'Is the electric motors rated output between 0.73kW and 185kW?',
+      'sorting' : 6,
+      'eligibility_clause' : """In PDRS SYS1 Implementation Requirements Clause 2 it states that the electric motor must have a rated output from 0.73kW to <185kW."""
+    }
---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/F7_PDRSAug24/activity_eligibility/F7_PDRSAug24_eligibility.py
@@ -0,0 +1,30 @@
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+import numpy as np
+
+
+class SYS1_installation_final_activity_eligibility(Variable):
+    """
+        Formula to calculate the SYS1 installation activity eligibility
+    """
+    value_type = bool
+    entity = Building
+    definition_period = ETERNITY
+    metadata = {
+       "variable-type": "output"
+    }
+
+    def formula(buildings, period, parameter):
+      new_motor_ventilation_refrigeration = buildings('SYS1_new_motor_for_ventilation_refrigeration', period)
+      registered_GEMS = buildings('SYS1_equipment_registered_in_GEMS', period)
+      high_efficiency = buildings('SYS1_high_efficiency', period)
+      equipment_installed = buildings('SYS1_equipment_installed', period)
+      ACP_engaged = buildings('SYS1_engaged_ACP', period)
+      rated_output_eligible = buildings('SYS1_rated_output_eligible', period)
+
+      end_formula = ( new_motor_ventilation_refrigeration * registered_GEMS * high_efficiency * 
+                      equipment_installed * ACP_engaged * rated_output_eligible )
+
+      return end_formula

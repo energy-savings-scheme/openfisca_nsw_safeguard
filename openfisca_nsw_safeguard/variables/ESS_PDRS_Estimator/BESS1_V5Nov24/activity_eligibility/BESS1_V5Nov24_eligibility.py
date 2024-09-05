6c55e6b123c853d94e1ab209049b83c52e23b355
Kate McKay <hello@katemckay.ca>
2024-09-05 10:14:38 +1000

    [DG22-1642] BESS1 eligibility built all tests passing

---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/BESS1_V5Nov24/activity_eligibility/BESS1_V5Nov24_eligibility.py
@@ -0,0 +1,45 @@
+from distutils.command.build import build
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+import numpy as np
+
+
+class BESS1_V5Nov24_installation_final_activity_eligibility(Variable):
+    """
+        Formula to calculate the BESS1 installation activity eligibility
+    """
+    value_type = bool
+    entity = Building
+    definition_period = ETERNITY
+    metadata = {
+        "alias": "BESS1 activity installation eligibility requirements",
+        "variable-type": "output"
+    }
+
+    def formula(buildings, period, parameter):
+       equipment_installation = buildings('BESS1_V5Nov24_new_installation', period)
+       new_solar_battery = buildings('BESS1_V5Nov24_new_solar_battery', period)
+       solar_panels = buildings('BESS1_V5Nov24_solar_panels', period)
+       ACP_engaged = buildings('BESS1_V5Nov24_engaged_ACP', period)
+       minimum_payment = buildings('BESS1_V5Nov24_minimum_payment', period)
+       battery_capacity = buildings('BESS1_V5Nov24_battery_capacity', period)
+       battery_warranty_length = buildings('BESS1_V5Nov24_length_battery_warranty', period)
+       battery_warranty_retainable_capacity = buildings('BESS1_V5Nov24_retainable_battery_capacity_warranty', period)
+       battery_warranty_temperature_range = buildings('BESS1_V5Nov24_temperature_range_warranty', period)
+       battery_warranty_throughput_before_April_2026 = buildings ('BESS1_V5Nov24_minimum_throughput_warranty_before_April_2026', period)
+       installation_location_eligible = buildings('BESS1_V5Nov24_installation_location_eligible', period)
+       installed_indoors_has_smoke_alarm = buildings('BESS1_V5Nov24_installed_indoors_has_smoke_alarm', period)
+       battery_internet_connectable = buildings('BESS1_V5Nov24_battery_internet_connectable', period)
+       battery_controllable_third_party = buildings('BESS1_V5Nov24_battery_controllable_third_party', period)
+       approved_product_list = buildings('BESS1_V5Nov24_approved_product_list', period)
+       approved_installer = buildings('BESS1_V5Nov24_approved_installer', period)
+       DER_Register = buildings('BESS1_V5Nov24_DER_register', period)
+
+       end_formula = ( equipment_installation * new_solar_battery * solar_panels * ACP_engaged * minimum_payment * battery_capacity * 
+                       battery_warranty_length * battery_warranty_retainable_capacity * battery_warranty_temperature_range * battery_warranty_throughput_before_April_2026 *
+                       installation_location_eligible * installed_indoors_has_smoke_alarm * battery_internet_connectable * battery_controllable_third_party * 
+                       approved_product_list * approved_installer * DER_Register)
+
+       return end_formula

c4810bb9cdd4d1eb1c1bceeb8b0abffc98414a84
kororo <kororo@users.noreply.github.com>
2025-02-25 15:57:45 +1100

    Release 20250225 (#477)

---
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/F16_electric_PDRSDec24/activity_eligibility/F16_electric_PDRSDec24_eligibility.py
@@ -0,0 +1,36 @@
+from distutils.command.build import build
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.indexed_enums import Enum
+from openfisca_nsw_base.entities import Building
+import numpy as np
+
+class F16_electric_PDRSDec24__installation_replacement_final_activity_eligibility(Variable):
+    """
+        Formula to calculate the WH1 replacement activity eligibility
+    """
+    value_type = bool
+    entity = Building
+    definition_period = ETERNITY
+    metadata = {
+        "variable-type": "output"
+    }
+
+    def formula(buildings, period, parameters):
+        replacement = buildings('F16_electric_PDRSDec24__equipment_replaced', period)
+        existing_equipment_removed = buildings('F16_electric_PDRSDec24__existing_equipment_removed', period)
+        equipment_installed_on_site = buildings('F16_electric_PDRSDec24__equipment_installed_on_site', period)
+        qualified_install_removal = buildings('F16_electric_PDRSDec24__qualified_install_removal', period)
+        engaged_ACP = buildings('F16_electric_PDRSDec24__engaged_ACP', period)
+        minimum_payment = buildings('F16_electric_PDRSDec24__minimum_payment', period)
+        building_class_1_or_4 = buildings('F16_electric_PDRSDec24__building_BCA_class_1_or_4', period)
+        certified_4234 = buildings('F16_electric_PDRSDec24__4234_certified', period)
+        scheme_admin_approved = buildings('F16_electric_PDRSDec24__scheme_admin_approved', period)
+        minimum_annual_energy = buildings('F16_electric_PDRSDec24__minimum_annual_energy', period)
+        storage_volume_certified = buildings('F16_electric_PDRSDec24__certified_and_eligible', period)
+        
+        end_formula = ( replacement * existing_equipment_removed * equipment_installed_on_site * qualified_install_removal
+                        * engaged_ACP * minimum_payment * certified_4234 * scheme_admin_approved * minimum_annual_energy
+                        * np.logical_not(building_class_1_or_4) * storage_volume_certified )
+
+        return end_formula

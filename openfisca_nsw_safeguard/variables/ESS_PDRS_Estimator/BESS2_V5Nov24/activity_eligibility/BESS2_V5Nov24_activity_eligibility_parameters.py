42a3c92e5b64c1de810e688afcc6a7ca8d57e73a
Felix Cahaya <fcahaya@kororo.net>
2025-06-09 11:51:26 +0700

    [DG22-2396] update BESS2 rule 3 eligibility

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/BESS2_V5Nov24/activity_eligibility/BESS2_V5Nov24_activity_eligibility_parameters.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/BESS2_V5Nov24/activity_eligibility/BESS2_V5Nov24_activity_eligibility_parameters.py
@@ -12,9 +12,9 @@ class BESS2_V5Nov24_demand_response_contract(Variable):
     default_value = True
     definition_period = ETERNITY
     metadata = {
-        'display_question' : 'Are you aware that you need to sign a demand response contract for your solar battery?',
+        'display_question' : 'Have you connected a solar battery to a virtual power plant?',
         'sorting' : 1,
-        'eligibility_clause' : """The PDRS BESS2 activity is to sign a behind the meter Battery Energy Storage System up to a demand response contract."""
+        'eligibility_clause' : """The PDRS BESS2 activity is onboard a behind the meter residential battery energy storage system with a demand response aggregator."""
     }
 
 

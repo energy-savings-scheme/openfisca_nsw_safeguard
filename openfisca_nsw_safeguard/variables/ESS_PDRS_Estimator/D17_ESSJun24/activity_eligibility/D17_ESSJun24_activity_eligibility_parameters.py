a32adca3075968c0d45f5fd5f64b86efd5d0e32d
SelvaRC <selva.chettiar@environment.nsw.gov.au>
2025-08-26 09:20:24 +1000

    D17 Eligibility Clause change from F16 to D17

--- openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/D17_ESSJun24/activity_eligibility/D17_ESSJun24_activity_eligibility_parameters.py
+++ openfisca_nsw_safeguard/variables/ESS_PDRS_Estimator/D17_ESSJun24/activity_eligibility/D17_ESSJun24_activity_eligibility_parameters.py
@@ -119,5 +119,5 @@ class D17_ESSJun24_safety_requirement(Variable):
     metadata = {
       'display_question' : 'Have safety requirements of AS/NZS 5149.3:2016 and manufacturer installation recommendations been followed for the installation?',
       'sorting' : 9,
-      'eligibility_clause' : """In ESS F16 Implementation Requirements Clause 4 it states that the where the replacement End-User Equipment is a split system with refrigerant flows between the evaporator and tank, safety requirements of AS/NZS 5149.3:2016 and manufacturer installation recommendations must be followed."""
+      'eligibility_clause' : """In ESS D17 Implementation Requirements Clause 4 it states that the where the replacement End-User Equipment is a split system with refrigerant flows between the evaporator and tank, safety requirements of AS/NZS 5149.3:2016 and manufacturer installation recommendations must be followed."""
     }
